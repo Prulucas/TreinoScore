5d6bfb1ee71a205b9c7d865f5755e283f848067e
TreinoScore <tg.treinoscore.fatec@gmail.com>
2025-04-27 10:08:18 -0300

    Refact: Banco de dados hospedado

--- src/database/db.js
+++ src/database/db.js
@@ -9,12 +9,25 @@ const sequelizeInstance = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST,  // Aqui será o URL do Supabase
         port: process.env.DB_PORT,
         dialect: 'postgres',
-        logging: console.log // Para ver os logs SQL
+        logging: console.log  // Para ver os logs SQL
     }
 );
 
-// 2. Exporte explicitamente como padrão
+// 2. Verifique a conexão
+const testConnection = async () => {
+    try {
+        await sequelizeInstance.authenticate();
+        console.log('Conexão com o Supabase foi bem-sucedida!');
+    } catch (error) {
+        console.error('Não foi possível conectar ao Supabase:', error);
+    }
+};
+
+// Chama a função de teste de conexão
+testConnection();
+
+// 3. Exporte explicitamente como padrão
 export default sequelizeInstance;
